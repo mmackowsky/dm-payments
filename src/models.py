5e5718597bbbb612a120f931dcd5bb6d848af7a4
mmackowsky <maciostah@gmail.com>
2024-04-24 18:48:07 +0200

    feat(models): add Payment model

---
+++ src/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from database import Base
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    user = Column(Integer, nullable=False)
+    amount = Column(Integer, nullable=False)
+    currency = Column(String, nullable=False)
+    date = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    status = Column(Boolean, nullable=False)
