796002b726d7ae8d5b0a7ec6a78aaeb7ed12bc14
mmackowsky <maciostah@gmail.com>
2024-05-08 19:00:16 +0200

    feat: add new util

--- src/utils.py
+++ src/utils.py
@@ -31,3 +31,17 @@ def set_new_id(db: SessionLocal):
     last_object_id = db.query(Subscription).order_by(desc(Subscription.id)).first()
     next_id = (last_object_id.id + 1) if last_object_id.id else 1
     return next_id
+
+
+def delete_last_item(db):
+    try:
+        last_item = db.query(Subscription).order_by(Subscription.id.desc()).first()
+
+        if last_item:
+            db.delete(last_item)
+            db.commit()
+            return {"message": "Last element has been removed."}
+        else:
+            return {"message": "No elements to remove."}
+    finally:
+        db.close()
